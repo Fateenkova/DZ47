79ffd15fc910ded7ebf24ea4e8096ddbfd874930
Helen <zybina.e@mail.ru>
2023-01-20 07:54:14 +0300

    задача 47

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 47 Задайте двумерный массив размером m*n,заполненый случайными вещественными числами.
+double[,] array = new double[3, 4];
+for (int i = 0; i < 3; i++)
+{
+    for (int j = 0; j < 4; j++)
+    {
+        array[i, j] = new Random().NextDouble();
+        Console.Write($"{array[i, j]}");
+    }
+    Console.WriteLine();
+}
